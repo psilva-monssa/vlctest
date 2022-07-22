99a4c773eba1aa64fbf9e8998a944ad97f473e22
elgodolfredo <psilva@monssa.com.ar>
2022-07-22 11:14:53 -0300

    all changes

--- App.js
+++ App.js
@@ -1,20 +1,109 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  Dimensions 
+} from 'react-native';
 
-export default function App() {
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+import { VLCPlayer } from '@psilva-monssa/react-native-vlc-media-player';
+
+const calcVLCPlayerHeight = (windowWidth,aspetRatio) => {
+  return windowWidth * aspetRatio;
+};
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {/* <StatusBar barStyle="dark-content" /> */}
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          {/* <Header title="test"/> */}
+          <View >
+              <Text style={styles.heading}>RN VLC Media Player</Text>
+          </View>
+          <View style={styles.body}>
+          <VLCPlayer
+            source={{
+              initType: 2,
+              hwDecoderEnabled: 1,
+              hwDecoderForced: 1,
+              uri:
+                'rtsp://psilva:q22334455@10.20.80.210/Streaming/Channels/502',
+              initOptions: [
+                '--no-audio',
+                '--rtsp-tcp',
+                '--network-caching=150',
+                '--rtsp-caching=150',
+                '--no-stats',
+                '--tcp-caching=150',
+                '--realrtsp-caching=150',
+              ],
+            }}
+            autoplay={true}
+            autoAspectRatio={true}
+            resizeMode="cover" 
+            // videoAspectRatio={"4:3"}
+            isLive={true}
+            autoReloadLive={true}
+            style={{ height: calcVLCPlayerHeight( Dimensions.get('window').width, 3/4), marginTop: 30}}
+          />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  heading:{
+    fontSize: 30,
+    fontWeight: '700',
+    color: Colors.black,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
   },
 });
+
+export default App;
